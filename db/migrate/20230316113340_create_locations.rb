0dfd87faaec7674dd71d743bd5af8c1be98adfe9
Dusan Orlovic <dule@hey.com>
2023-03-16 12:50:05 +0100

    Generate Location, Group, Wish and WishGroup model #6
    rails g model location name address lat latitude longitude url_for_map
    rails g model group location:references name age:integer
    rails g model wish group:references user:references
    rails g model wish_group wish:references group:references

---
+++ db/migrate/20230316113340_create_locations.rb
@@ -0,0 +1,14 @@
+class CreateLocations < ActiveRecord::Migration[7.0]
+  def change
+    create_table :locations do |t|
+      t.string :name
+      t.string :address
+      t.string :lat
+      t.string :latitude
+      t.string :longitude
+      t.string :url_for_map
+
+      t.timestamps
+    end
+  end
+end
