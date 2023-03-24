968e30732d3d6770dada505fae34975eb526e8e6
VuDej <drvu47@gmail.com>
2023-03-24 12:06:53 +0100

    fix standard errors

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-if ['development', 'test'].include? ENV['RAILS_ENV']
+if ["development", "test"].include? ENV["RAILS_ENV"]
   Dotenv::Railtie.load
 end
 
@@ -28,7 +28,6 @@ module KindergartenExchange
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
 
     # Configuration for the application, engines, and railties goes here.
     #
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,16 +63,15 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  
+
   config.action_mailer.smtp_settings = {
-    port: ENV.fetch('SMTP_PORT'),
-    address: ENV.fetch('SMTP_SERVER'),
-    user_name: ENV.fetch('SMTP_LOGIN'),
-    password: ENV.fetch('SMTP_PASSWORD'),
-    domain: ENV.fetch('SMTP_DOMAIN'),
+    port: ENV.fetch("SMTP_PORT"),
+    address: ENV.fetch("SMTP_SERVER"),
+    user_name: ENV.fetch("SMTP_LOGIN"),
+    password: ENV.fetch("SMTP_PASSWORD"),
+    domain: ENV.fetch("SMTP_DOMAIN"),
     authentication: :plain,
     enable_starttls_auto: true
   }
   config.action_mailer.delivery_method = :smtp
-  
 end
