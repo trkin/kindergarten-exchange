1714df9ff1906816f2304764380fc3d5fc88c581
VuDej <drvu47@gmail.com>
2023-03-24 01:55:36 +0100

    adding necessary files for spec directory

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,6 +2,8 @@
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
+require_relative 'support/factory_bot'
+require_relative 'support/chrome'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
