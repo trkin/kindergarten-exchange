66fdea753333a34f50d70c19abbd650f73eceec3
VuDej <drvu47@gmail.com>
2023-03-27 08:05:23 +0200

    active admin fix, login throut user model. Added models to active-admin

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,6 +3,7 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
   content title: proc { I18n.t("active_admin.dashboard") } do
+    button link_to sanitize("&larr; Back to app"), root_path
     div class: "blank_slate_container", id: "dashboard_default_message" do
       span class: "blank_slate" do
         span I18n.t("active_admin.dashboard_welcome.welcome")
