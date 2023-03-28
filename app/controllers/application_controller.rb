862ec3a4547c81fd5813436697d717a925f107f0
VuDej <drvu47@gmail.com>
2023-03-28 12:32:52 +0200

    Install gem "activeadmin" without Admin users. Add column admin to users table and flag on user model. #i28

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  helper_method :resource
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:email, :password, :password_confirmation)
+    end
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:email, :password, :password_confirmation)
+    end
+  end
+
+  def authenticate_admin!
+    unless current_user
+      redirect_to root_path and return
+    end
+    redirect_to new_user_session_path unless current_user.is_admin?
+  end
 end
