ef4cd1e3e381360ec1a50d3ee3fd89c8c944f1ec
VuDej <drvu47@gmail.com>
2023-03-29 08:29:59 +0200

    User validation and test for model User

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'email, password validation' do
+    subject { build(:user) }
+
+    context 'when valid email' do
+      let(:email) { Faker::Internet.email }
+      it { should  have_db_column(:email) }
+      it { should  have_db_index(:email).unique }
+      it { should  validate_presence_of(:email) }
+      it { should  allow_value(email).for(:email) }
+    end
+    context 'when valid password' do
+      let(:password) { Faker::Internet.password }
+      it { should  validate_presence_of(:password) }
+      it { should  allow_value(password).for(:password) }
+    end
+  end
+end
