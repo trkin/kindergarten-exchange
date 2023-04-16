1ceb127a057d7770b83f251827a36b19d057de43
Milan Kecman <milan.kecman270@gmail.com>
2023-04-16 19:12:05 +0200

    [#34] Added test folder and example for testing

---
+++ app/javascript/test/sum.test.js
@@ -0,0 +1,14 @@
+const sum = require('./sum');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+test('adds 1 + 4 to equal 5', () => {
+  expect(sum(1, 4)).toBe(5);
+});
