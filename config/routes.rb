995ee793537725b439f60a05edf381784f107750
vesna <vesna.lesevic85@gmail.com>
2023-05-11 12:20:01 +0200

    [#4] Contact form using ActiveModel

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   get "sign-in-development/:id", to: "pages#sign_in_development", as: :sign_in_development
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
+
   root "pages#index"
+  get "contact", to: "pages#contact_show"
+  post "contact", to: "pages#contact_create", as: "contact_submit"
 end
